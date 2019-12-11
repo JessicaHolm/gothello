2c443065cc0100eb61814da0feb9cd12e2053dba
Jason Holm <holmjason2@gmail.com>
2019-12-10 16:33:08 -0800

    added comments to table

--- table.py
+++ table.py
@@ -1,7 +1,12 @@
+# Class that manages the transposition table.
+# Code adapted from the Wikipedia page for Zobrist hashing.
+# https://en.wikipedia.org/wiki/Zobrist_hashing
+
 import random
 
 class Table(object):
     
+    # Create a array of 20 bits for all possible combinations of piece and position.
     def __init__(self):
         bit_table = [[0] * 2 for _ in range(25)]
         for i in range(25):
@@ -10,6 +15,7 @@ class Table(object):
         self.bit_table = bit_table
         self.table = {}
 
+    # Use the Zobrist hash to get a hash of the board position.
     def get_hash(self, grid):
         h = 0
         count = 0
@@ -21,6 +27,7 @@ class Table(object):
                 count += 1
         return h
 
+    # Check to see if a position is in the table.
     def ttLookup(self, grid):
         key = self.get_hash(grid)
         if key in self.table:
@@ -28,12 +35,14 @@ class Table(object):
         else:
             return Entry()
 
+    # Store a position indexed by its hash.
     def ttStore(self, grid, ttEntry):
         key = self.get_hash(grid)
         self.table[key] = ttEntry
 
 class Entry(object):
 
+    # Initalize values for a table entry.
     def __init__(self, value = 0, flag = 0, depth = -1):
         self.value = value
         self.flag = flag
